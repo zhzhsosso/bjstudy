b5aebddedc9583eb6b23700615cb2e241fef3c6e
zhzhsosso <sally1248@naver.com>
2022-08-16 17:46:54 +0900

    구구단_2739

---
+++ src/study/gugudan_2739.java
@@ -0,0 +1,15 @@
+package study;
+
+import java.util.Scanner;
+
+public class gugudan_2739 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+	
+		for(int i=1; i<10; i++){
+			System.out.println(num + " * " + i + " = " + num*i);
+		}
+		sc.close();
+	}
+}
