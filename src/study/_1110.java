00a3ac6d1c8fb042751013b2e122627bc8763ec3
zhzhsosso <sally1248@naver.com>
2022-08-16 22:51:26 +0900

    더하기 사이클_1110

---
+++ src/study/_1110.java
@@ -0,0 +1,25 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1110 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt =0;
+		int copy = N;
+		
+		do {
+			N = ((N%10)*10) + (((N/10) + (N%10)) % 10);
+			cnt++;
+		
+		}while(copy != N);
+		System.out.println(cnt);
+
+	}
+}
