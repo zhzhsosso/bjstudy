f21be37cedbb4d7f09d344738951cdb998566d71
zhzhsosso <sally1248@naver.com>
2022-09-27 21:57:39 +0900

    1차원배열 - 나머지,평균

--- src/study/_1546.java
+++ src/study/_1546.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class _1546 {
 
 	public static void main(String[] args) throws IOException{
--- src/study/_3052.java
+++ src/study/_3052.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 
 public class _3052 {
 
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		HashSet<Integer> h = new HashSet<Integer>();
